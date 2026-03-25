f2693cefa34f3ce3dcf057257645b248cbc6d25a
Robin Play <RustRobin.ru@yandex.ru>
2026-03-25 21:00:00 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,18 +1,4 @@
 @echo off
-chcp 65001 >nul
 cd /d "%~dp0"
-
-echo --- Обновление rust-template с GitHub ---
-
-echo 1. Сохранение локальных изменений (stash)...
-git stash
-
-echo 2. Получение обновлений (pull)...
-git pull origin main
-
-echo 3. Восстановление локальных изменений (stash pop)...
-git stash pop
-
-echo.
-echo Готово! Ваши плагины в папке 'plugins/' в безопасности, а файлы шаблона и DLL обновлены.
+powershell -NoProfile -ExecutionPolicy Bypass -File "update.ps1"
 pause
