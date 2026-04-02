0a5ed9caf1d314d1e3ffad81bb0d89d707188094
Robin Play <RustRobin.ru@yandex.ru>
2026-04-03 01:27:41 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,6 @@
 @echo off
-:: RUST-TEMPLATE LAUNCHER
+chcp 65001 >nul
+cd /d "%~dp0"
 echo ==========================================
 echo    RUST-TEMPLATE UPDATE TOOL
 echo ==========================================
