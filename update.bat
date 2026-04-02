139fe98132717a3edf78156efbd60b85456a6efd
Robin Play <RustRobin.ru@yandex.ru>
2026-04-03 01:31:59 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,8 +1,5 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-echo ==========================================
-echo    RUST-TEMPLATE UPDATE TOOL
-echo ==========================================
 powershell -NoProfile -ExecutionPolicy Bypass -File "update.ps1"
 pause
